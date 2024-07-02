788818afbe026fb91caca15ff7eea72887627267
Nick Lim <nicklimvs@gmail.com>
2024-07-02 18:17:04 +0800

    add search bar

--- src/modules/main/header/fullscreen.vue
+++ src/modules/main/header/fullscreen.vue
@@ -13,36 +13,38 @@
   
     const toggleFullscreen = () => {
         if (!isFullscreen.value) {
-        enterFullscreen();
+            enterFullscreen();
         } else {
-        exitFullscreen();
+            exitFullscreen();
         }
     };
   
     const enterFullscreen = () => {
         const elem = document.documentElement;
         if (elem.requestFullscreen) {
-        elem.requestFullscreen();
-        } else if (elem.mozRequestFullScreen) { // Firefox
-        elem.mozRequestFullScreen();
-        } else if (elem.webkitRequestFullscreen) { // Chrome, Safari and Opera
-        elem.webkitRequestFullscreen();
-        } else if (elem.msRequestFullscreen) { // IE/Edge
-        elem.msRequestFullscreen();
-        }
+            elem.requestFullscreen();
+        } 
+        // else if (elem.mozRequestFullScreen) { // Firefox
+        //     elem.mozRequestFullScreen();
+        // } else if (elem.webkitRequestFullscreen) { // Chrome, Safari and Opera
+        //     elem.webkitRequestFullscreen();
+        // } else if (elem.msRequestFullscreen) { // IE/Edge
+        //     elem.msRequestFullscreen();
+        // }
         isFullscreen.value = true;
     };
   
     const exitFullscreen = () => {
         if (document.exitFullscreen) {
-        document.exitFullscreen();
-        } else if (document.mozCancelFullScreen) { // Firefox
-        document.mozCancelFullScreen();
-        } else if (document.webkitExitFullscreen) { // Chrome, Safari and Opera
-        document.webkitExitFullscreen();
-        } else if (document.msExitFullscreen) { // IE/Edge
-        document.msExitFullscreen();
-        }
+            document.exitFullscreen();
+        } 
+        // else if (document.mozCancelFullScreen) { // Firefox
+        //     document.mozCancelFullScreen();
+        // } else if (document.webkitExitFullscreen) { // Chrome, Safari and Opera
+        //     document.webkitExitFullscreen();
+        // } else if (document.msExitFullscreen) { // IE/Edge
+        //     document.msExitFullscreen();
+        // }
         isFullscreen.value = false;
     };
 </script>
---
+++ src/modules/main/header/search.vue
@@ -0,0 +1,51 @@
+<template>
+    <li class="nav-item">
+        <div class="nav-link" @click="toggleSearch">
+            <i class="fas fa-search"></i>
+        </div>
+        <div v-if="isSearchVisible" class="navbar-search-block">
+            <form class="form-inline" @submit.prevent="handleSubmit">
+                <div class="input-group input-group-sm">
+                    <input
+                        v-model="searchQuery" 
+                        class="form-control form-control-navbar" 
+                        type="search" 
+                        placeholder="Search" 
+                        aria-label="Search"
+                    />
+                    <div class="input-group-append">
+                        <button class="btn btn-outline-primary" type="submit">
+                            <i class="fas fa-search"></i>
+                        </button>
+                        <button class="btn btn-outline-primary" type="button"  @click="toggleSearch">
+                            <i class="fas fa-times"></i>
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </li>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const isSearchVisible = ref(false);
+const searchQuery = ref('');
+
+const toggleSearch = () => {
+  isSearchVisible.value = !isSearchVisible.value;
+};
+
+const handleSubmit = () => {
+  console.log('Search query:', searchQuery.value);
+  // Add your search handling logic here
+};
+</script>
+
+<style scoped>
+.navbar-search-block {
+  display: flex;  
+  background: #ffffff;
+}
+</style>
