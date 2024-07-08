2021f674499e16449ed20d2ea8180cacb9671002
Nick Lim <nicklimvs@gmail.com>
2024-07-08 16:12:11 +0800

    update output files

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import path, { format } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig(({mode }) => {
@@ -15,6 +15,23 @@ export default defineConfig(({mode }) => {
         '@components': path.resolve(__dirname, './src/components'),
         '@pages': path.resolve(__dirname, './src/pages')
       }
+    },
+    build: {
+      outDir: 'dist',
+      lib: {
+        entry: path.resolve(__dirname, './src/main.ts'),
+        name: 'AdminLteVue3',
+        formats: ['umd', 'es'],
+        fileName: (format) => `adminlte-vue3.${format}.min.js`,
+      },
+      rollupOptions: {
+        external: ['vue'],
+        output: {
+          globals: {
+            vue: 'Vue',
+          }
+        }
+      },
     }
   }
 })
