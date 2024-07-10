b9c9fda60a57313d42ab6332b0fa600c15c8ae9c
Nick Lim <nicklimvs@gmail.com>
2024-07-10 17:17:27 +0800

    Export Footer.vue

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as Card } from './card.vue';
 export { default as DualListbox } from './dual-listbox.vue';
+export { default as Footer } from './Footer.vue';
--- src/components/main.ts
+++ src/components/main.ts
@@ -1,11 +1,12 @@
 import type { App } from 'vue';
-import { Card, DualListbox } from '@/components';
+import { Card, DualListbox, Footer } from '@/components';
 
 export default {
     install: (app: App) => {
         app.component('Card', Card);
         app.component('DualListbox', DualListbox);
+        app.component('Footer');
     }
 };
 
-export { Card, DualListbox }
+export { Card, DualListbox, Footer }
