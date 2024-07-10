4234d1b89d3939316c838d81a4f44f60c1608247
Nick Lim <nicklimvs@gmail.com>
2024-07-10 15:35:28 +0800

    remove unused imports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,2 @@
-import Card from './card.vue';
-import DualListbox from './dual-listbox.vue';
-
 export { default as Card } from './card.vue';
 export { default as DualListbox } from './dual-listbox.vue';
