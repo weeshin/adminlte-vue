7f6cff14f76b978d0f387a97fa9574fd15d74417
Nick Lim <nicklimvs@gmail.com>
2024-07-24 14:32:04 +0800

    Update ActionableDataTable.vue

--- src/components/ActionableDataTable.vue
+++ src/components/ActionableDataTable.vue
@@ -195,6 +195,7 @@ const getComponent = (type: string) => {
 const toast = useToast();
 const showToast = (message: string) => {      
       toast.success(message);      
+      console.log("ActionableDatable showToast");
 };
 
 const submitForm = () => {    
