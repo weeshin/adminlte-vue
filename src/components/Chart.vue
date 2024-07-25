b044257fbe319237d06511a4480b43fb3693e197
Nick Lim <nicklimvs@gmail.com>
2024-07-25 15:54:52 +0800

    Create Chart.vue

---
+++ src/components/Chart.vue
@@ -0,0 +1,80 @@
+<template>
+
+<div class="row">
+    <div class="col-md-8">
+        <div class="chart-responsive">
+            <canvas ref="chart" height="150"></canvas>
+        </div>
+        <!-- ./chart-responsive -->
+    </div>
+
+    <div class="col-md-4">
+        <ul class="chart-legend clearfix">
+            <li v-for="(label, index) in props.labels" :key="label">
+                <i class="far fa-circle" :style="{ color: props.backgroundColors[index] }"></i> {{ label }}
+            </li>
+        </ul>
+    </div>
+</div>
+
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref, nextTick } from 'vue';
+import { Chart, registerables } from 'chart.js';
+
+Chart.register(...registerables);
+
+// Define props
+const props = defineProps<{
+  labels: string[],
+  data: number[],
+  backgroundColors: string[]
+}>();
+
+const chart = ref<HTMLCanvasElement | null>(null);
+
+onMounted(async () => {
+    await nextTick();
+
+    if (chart.value) {
+
+        const ctx = chart.value.getContext('2d');
+        if(ctx) {
+            new Chart(ctx, {
+                type: 'doughnut',
+                data: {
+                    labels: props.labels,
+                    datasets: [{
+                        data: props.data,
+                        backgroundColor: props.backgroundColors
+                    }]
+                },
+                options: {
+                    responsive: true,
+                    maintainAspectRatio: false,
+                    plugins: {
+                        legend: {
+                            display: false
+                        }
+                    }
+                }
+            });
+        } else {
+            console.error('Failed to get 2D context from canvas.');
+        }
+        
+    }   else {
+        console.error('Canvas element not found.');
+    }
+
+});
+
+</script>
+
+<style scoped>
+.chart-responsive {
+  position: relative;
+  height: 150px;
+}
+</style>
