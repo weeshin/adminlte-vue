75f6f99581f1dc1c4af9ba1c96c67b8c6b446801
Nick Lim <nicklimvs@gmail.com>
2024-08-17 16:52:33 +0800

    format code

--- src/pages/Calendars.vue
+++ src/pages/Calendars.vue
@@ -1,24 +1,21 @@
 <template>
-    <div class="wrapper">
-      <MonthlyGrid :initial-year="2024" :initial-month="7" :records="data" />
-    </div>
-  </template>
-  
-  <script setup lang="ts">
-  import { ref, onMounted } from 'vue';
-  import MonthlyGrid from '@components/Attendance.vue';
-  import axios from 'axios';
-  
-  const data = ref([]);
-  onMounted(async () => {
-    try {
-      const response = await axios.get('/data/attendances.json');
-      data.value = response.data;      
-    } catch(error) {
-      console.error('Error fetching', error);
-    }
-  });
+  <div class="wrapper">
+    <MonthlyGrid :initial-year="2024" :initial-month="7" :records="data" />
+  </div>
+</template>
 
-  
-  </script>
-  
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+import MonthlyGrid from "@components/Attendance.vue";
+import axios from "axios";
+
+const data = ref([]);
+onMounted(async () => {
+  try {
+    const response = await axios.get("/data/attendances.json");
+    data.value = response.data;
+  } catch (error) {
+    console.error("Error fetching", error);
+  }
+});
+</script>
