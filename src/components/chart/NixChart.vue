5c2d4d1e470044b3948c3b849fe1e5f78bfccd3a
Nick Lim <nicklimvs@gmail.com>
2024-09-22 17:57:58 +0800

    add Chart.js

---
+++ src/components/chart/NixChart.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <canvas :id="props.id"  width="200" height="100"></canvas>
+  </div>
+</template>
+
+
+<script setup lang="ts">
+import { onMounted, defineProps } from 'vue';
+import { Chart, registerables, ChartType, ChartData, ChartOptions } from 'chart.js';
+
+const props = defineProps<{
+  id: string,
+  type?: ChartType,
+  options?: ChartOptions,
+  data: ChartData
+}>();
+
+// Register all components including scales
+Chart.register(...registerables);
+
+onMounted(() => {
+  const ctx = document.getElementById(props.id) as HTMLCanvasElement;
+  
+  new Chart(ctx, {
+    type: props.type || 'bar', // or 'line', 'pie', 'doughnut', etc.
+    data: props.data,
+    options: props.options
+  });
+
+});
+</script>
+
+<style scoped>
+canvas {
+  max-width: 100%;
+  max-height: 400px;
+}
+</style>
