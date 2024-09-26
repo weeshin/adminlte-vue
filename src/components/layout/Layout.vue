1a4a77d8f0af99a65a9f7512cc701de944ec60a9
Nick Lim <nicklimvs@gmail.com>
2024-09-26 16:42:13 +0800

    support mobile view

--- src/components/layout/Layout.vue
+++ src/components/layout/Layout.vue
@@ -1,11 +1,11 @@
 <template>
-  <div :class="['wrapper']">
+  <div :class="['wrapper']" @click="handleOutsideClick">
     <!-- Navbar -->
     <nav class="main-header navbar navbar-expand navbar-white navbar-light">
       <!-- Left navbar links -->
       <ul class="navbar-nav">
         <li class="nav-item">
-            <button class="nav-link" role="button" @click="onToggleMenuSidebar">
+            <button class="nav-link" role="button" @click.stop="onToggleMenuSidebar">
                 <i class="fas fa-bars"></i>
             </button>
         </li>
@@ -19,7 +19,7 @@
     </nav>
 
     
-    <MenuSidebar :menu-items="menuItems" />    
+    <MenuSidebar ref="menuSidebar" :menu-items="menuItems" :class="['main-sidebar']"/>    
     
     <!-- Main Content -->
     <div :class="['content-wrapper']">
@@ -32,9 +32,11 @@
 </template>
 
 <script setup lang="ts">
-import { ref, defineProps } from 'vue';
+import { ref, defineProps, onMounted } from 'vue';
 import MenuSidebar from '@/components/layout/MenuSidebar.vue';
 import Footer from '@/components/layout/Footer.vue';
+
+const menuSidebar = ref();
 
 defineProps({
   menuItems: {
@@ -49,18 +51,39 @@ defineProps({
 
 const drawerVisible = ref(true); // Control the visibility of the drawer
 
+onMounted(async () => {
+  document.body.classList.add('sidebar-mini');  
+  document.body.classList.add('layout-fixed');    
+  drawerVisible.value = true;
+  menuSidebar.value?.offCollapse();      
+  menuSidebar.value?.onShow();     
+});
+
 const onToggleMenuSidebar = () => {  
   drawerVisible.value = !drawerVisible.value;
-
+  // console.log("onToggleMenuSidebar click ", drawerVisible.value);
   // Toggle the class on the body
-  if (drawerVisible.value) {
-    document.body.classList.remove('sidebar-collapse');
-  } else {
-    document.body.classList.add('sidebar-collapse');
+  if (drawerVisible.value) {    
+    menuSidebar.value?.offCollapse();      
+    menuSidebar.value?.onShow();   
+  } else {    
+    menuSidebar.value?.onCollapse();    
+    menuSidebar.value?.onHide();
   }
 };
 
-
+// Handle clicks outside the sidebar
+const handleOutsideClick = (event: MouseEvent) => {
+  const sidebar = menuSidebar.value?.$el;
+  if (sidebar && !sidebar.contains(event.target as Node)) {
+    // If the click is outside the sidebar, hide it
+    if (drawerVisible.value) {
+      drawerVisible.value = false;
+      // document.body.classList.add('sidebar-collapse');
+      menuSidebar.value?.onHide(); // Hide the sidebar
+    }
+  }
+};
 </script>
 
 <style scoped>
